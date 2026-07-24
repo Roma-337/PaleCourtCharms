c37195b639072268854ae3a025cda6a08380c613
Roma-337 <123456789az4135@gmail.Com>
2026-07-24 19:25:22 +0300

    Fix globalsettings bug with logic not applying at first launch
    This was actually a pretty stupid mistake but oh well.Fixed now what i've had scinse release lmao
    Expirience helps a lot (shocking)

--- Rando/LogicHandler.cs
+++ Rando/LogicHandler.cs
@@ -1,7 +1,10 @@
 using System;
+using System.IO;
 using System.Reflection;
+using Newtonsoft.Json;
 using RandomizerCore.Json;
 using RandomizerCore.Logic;
+using RandomizerMod.Logging;
 using RandomizerMod.RC;
 using RandomizerMod.Settings;
 
@@ -11,33 +14,46 @@ namespace PaleCourtCharms.Rando
     {
         internal static void Hook()
         {
-            if (PaleCourtCharms.GlobalSettings.AddCharms)
-            {
-
-
-                RCData.RuntimeLogicOverride.Subscribe(0f, ApplyLogic);
-            }
+            RCData.RuntimeLogicOverride.Subscribe(0f, ApplyLogic);
+            SettingsLog.AfterLogSettings += AddConnectionSettings;
         }
 
-     private static void ApplyLogic(GenerationSettings gs, LogicManagerBuilder lmb)
-{
-    if (!PaleCourtCharms.GlobalSettings.AddCharms)
-        return;
+        private static void ApplyLogic(GenerationSettings gs, LogicManagerBuilder lmb)
+        {
+            if (!PaleCourtCharms.GlobalSettings.AddCharms)
+                return;
 
-    Assembly asm = Assembly.GetExecutingAssembly();
-    JsonLogicFormat fmt = new();
+            Assembly asm = Assembly.GetExecutingAssembly();
+            JsonLogicFormat fmt = new();
 
-    using (var s = asm.GetManifestResourceStream("PaleCourtCharms.Rando.Terms.json"))
-        lmb.DeserializeFile(LogicFileType.Terms, fmt, s);
+            using (var s = asm.GetManifestResourceStream("PaleCourtCharms.Rando.Terms.json"))
+                lmb.DeserializeFile(LogicFileType.Terms, fmt, s);
 
+            using (var s = asm.GetManifestResourceStream("PaleCourtCharms.Rando.Locations.json"))
+                lmb.DeserializeFile(LogicFileType.Locations, fmt, s);
 
-    using (var s = asm.GetManifestResourceStream("PaleCourtCharms.Rando.Locations.json"))
-        lmb.DeserializeFile(LogicFileType.Locations, fmt, s);
+            using (var s = asm.GetManifestResourceStream("PaleCourtCharms.Rando.Items.json"))
+                lmb.DeserializeFile(LogicFileType.ItemStrings, fmt, s);
 
- 
-    using (var s = asm.GetManifestResourceStream("PaleCourtCharms.Rando.Items.json"))
-        lmb.DeserializeFile(LogicFileType.ItemStrings, fmt, s);
-}
+            // using (var s = asm.GetManifestResourceStream("PaleCourtCharms.Rando.LogicMacros.json"))
+            //     lmb.DeserializeFile(LogicFileType.MacroEdit, fmt, s);
 
+            // using (var s = asm.GetManifestResourceStream("PaleCourtCharms.Rando.LogicWaypoints.json"))
+            //     lmb.DeserializeFile(LogicFileType.Waypoints, fmt, s);
+
+            // using (var s = asm.GetManifestResourceStream("PaleCourtCharms.Rando.ConnectionLogicPatches.json"))
+            //     lmb.DeserializeFile(LogicFileType.LogicEdit, fmt, s);
+        }
+        //Log to settings.txt
+        private static void AddConnectionSettings(LogArguments args, TextWriter tw)
+        {
+            if (!PaleCourtCharms.GlobalSettings.AddCharms && !PaleCourtCharms.GlobalSettings.RandomizeCosts)
+                return;
+
+            tw.WriteLine("Logging Pale Court Charms Settings:");
+            using JsonTextWriter jtw = new(tw) { CloseOutput = false };
+            RandomizerMod.RandomizerData.JsonUtil._js.Serialize(jtw, PaleCourtCharms.GlobalSettings);
+            tw.WriteLine();
+        }
     }
 }
